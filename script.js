7df853fa70b99ad91fe3c0adb6cdd1cd1cc9ec24
Michael Selchow <mselchow@pm.me>
2022-04-09 15:33:30 -0400

    Add handler for all keys except operations and equals

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let displayValue = "";
+
 function add(a, b) {
   return a + b;
 }
@@ -16,13 +18,46 @@ function divide(a, b) {
 
 function operate(operator, a, b) {
   switch (operator) {
-    case "+":
+    case "add":
       return add(a, b);
-    case "-":
+    case "subtract":
       return subtract(a, b);
-    case "*":
+    case "multiply":
       return multiply(a, b);
-    case "/":
+    case "divide":
       return divide(a, b);
   }
 }
+
+document.querySelector(".calculator").addEventListener("click", function (e) {
+  let target = e.target;
+  if (target.matches("button")) {
+    let action = target.dataset.action;
+    let key = target.textContent;
+    if (!action) {
+      if (key === ".") {
+        if (!displayValue.includes(".")) {
+          displayValue += key;
+        }
+      } else if (key === "0") {
+        if (displayValue === "0") {
+          return;
+        } else if (displayValue.includes(".")) {
+          displayValue += key;
+        } else {
+          displayValue += key;
+        }
+      } else {
+        displayValue += key;
+      }
+    } else if (action === "clear") {
+      displayValue = "";
+    } else if (action === "sign") {
+      displayValue = multiply(displayValue, -1);
+    } else if (action === "percent") {
+      let result = operate("divide", parseFloat(displayValue), 100);
+      displayValue = result.toString();
+    }
+    document.querySelector("#display").value = displayValue;
+  }
+});
