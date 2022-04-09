1afe1638a33b2fd4fac211603338236262a31999
Michael Selchow <mselchow@pm.me>
2022-04-09 16:01:11 -0400

    Add functionality for digits and basic actions

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let displayValue = "";
+let currentOperator = "";
+let previousValue = "";
 
 function add(a, b) {
   return a + b;
@@ -34,30 +36,38 @@ document.querySelector(".calculator").addEventListener("click", function (e) {
   if (target.matches("button")) {
     let action = target.dataset.action;
     let key = target.textContent;
+
+    // Check for digits/decimal
     if (!action) {
       if (key === ".") {
         if (!displayValue.includes(".")) {
           displayValue += key;
         }
       } else if (key === "0") {
-        if (displayValue === "0") {
-          return;
-        } else if (displayValue.includes(".")) {
+        if (!displayValue.startsWith("0") || displayValue.length > 1) {
           displayValue += key;
+        }
+      } else if (displayValue.includes(".")) {
+        if (displayValue.length == 1) {
+          displayValue = "0" + displayValue + key;
         } else {
           displayValue += key;
         }
       } else {
         displayValue += key;
       }
-    } else if (action === "clear") {
+    }
+
+    // Check for actions
+    else if (action === "clear") {
       displayValue = "";
     } else if (action === "sign") {
       displayValue = multiply(displayValue, -1);
     } else if (action === "percent") {
       let result = operate("divide", parseFloat(displayValue), 100);
       displayValue = result.toString();
-    }
+    } // TODO: check for operators
+
     document.querySelector("#display").value = displayValue;
   }
 });
