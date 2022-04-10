b2fcdf79d6da15bca05e0359307f466ce4f0821e
Michael Selchow <mselchow@pm.me>
2022-04-10 08:17:53 -0400

    Clean up HTML, add some helper functions

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let displayValue = "";
-let currentOperator = "";
-let previousValue = "";
+let displayValue = "0";
+let firstOperand = "";
+let secondOperand = "";
+let operator = "";
 
 function add(a, b) {
   return a + b;
@@ -31,43 +32,91 @@ function operate(operator, a, b) {
   }
 }
 
-document.querySelector(".calculator").addEventListener("click", function (e) {
-  let target = e.target;
-  if (target.matches("button")) {
-    let action = target.dataset.action;
-    let key = target.textContent;
+function clearMemory() {
+  displayValue = "0";
+  firstOperand = "";
+  secondOperand = "";
+  operator = "";
+  updateDisplay();
+}
 
-    // Check for digits/decimal
-    if (!action) {
-      if (key === ".") {
-        if (!displayValue.includes(".")) {
-          displayValue += key;
-        }
-      } else if (key === "0") {
-        if (!displayValue.startsWith("0") || displayValue.length > 1) {
-          displayValue += key;
-        }
-      } else if (displayValue.includes(".")) {
-        if (displayValue.length == 1) {
-          displayValue = "0" + displayValue + key;
-        } else {
-          displayValue += key;
-        }
-      } else {
-        displayValue += key;
-      }
-    }
+function updateDisplay() {
+  document.getElementById("display").value = displayValue;
+}
 
-    // Check for actions
-    else if (action === "clear") {
-      displayValue = "";
-    } else if (action === "sign") {
-      displayValue = multiply(displayValue, -1);
-    } else if (action === "percent") {
-      let result = operate("divide", parseFloat(displayValue), 100);
-      displayValue = result.toString();
-    } // TODO: check for operators
+updateDisplay();
 
-    document.querySelector("#display").value = displayValue;
-  }
-});
+// document.querySelector(".calculator").addEventListener("click", function (e) {
+//   let target = e.target;
+//   if (target.matches("button")) {
+//     let action = target.dataset.action;
+//     let key = target.textContent;
+
+//     // Check for digits/decimal
+//     if (!action) {
+//       if (previousAction === "operator") {
+//         previousValue = displayValue;
+//         displayValue = "";
+//         previousAction = "digit";
+//       }
+//       if (key === ".") {
+//         if (!displayValue.includes(".")) {
+//           displayValue += key;
+//         }
+//       } else if (key === "0") {
+//         if (!displayValue.startsWith("0") || displayValue.length > 1) {
+//           displayValue += key;
+//         }
+//       } else if (displayValue.includes(".")) {
+//         if (displayValue.length == 1) {
+//           displayValue = "0" + displayValue + key;
+//         } else {
+//           displayValue += key;
+//         }
+//       } else {
+//         displayValue += key;
+//       }
+//     }
+
+//     // Check for actions
+//     else if (action === "clear") {
+//       displayValue = "";
+//       clearMemory();
+//     } else if (action === "sign") {
+//       displayValue = multiply(displayValue, -1);
+//       clearMemory();
+//     } else if (action === "percent") {
+//       let result = operate("divide", parseFloat(displayValue), 100);
+//       displayValue = result.toString();
+//       clearMemory();
+//     } else if (
+//       action === "add" ||
+//       action === "subtract" ||
+//       action === "multiply" ||
+//       action === "divide" ||
+//       action === "equals"
+//     ) {
+//       if (previousOperator === "") {
+//         previousOperator = action;
+//         previousValue = displayValue;
+//       } else {
+//         let result = operate(
+//           previousOperator,
+//           parseFloat(previousValue),
+//           parseFloat(displayValue)
+//         );
+//         displayValue = result.toString();
+
+//         if (action === "equals") {
+//           clearMemory();
+//         } else {
+//           previousOperator = action;
+//           previousValue = displayValue;
+//           displayValue = "";
+//         }
+//       }
+//     }
+
+//     document.querySelector("#display").value = displayValue;
+//   }
+// });
